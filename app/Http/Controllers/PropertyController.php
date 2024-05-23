17e815ba8647de4abd3d4d380cffd3372b0b3ce3
Pavan <pavan.sardjoepersad@gmail.com>
2024-05-23 18:32:58 +0200

    Added Dynamic Properties and Seeding

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
     public function index(){
-        return view('property.index');
+        $properties = Property::all();
+        return view('property.index', ['properties' => $properties]);
     }
 }
